9b1d6398e2ccbb430d3e5718798821c8721fd244
avazindel <ava.zindel@gmail.com>
2026-04-19 22:20:59 -0500

    Win UI
    added UI assets and win script

---
+++ Assets/scripts/WinZone.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class WinZone : MonoBehaviour
+{
+
+    public GameObject targetCanvas;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        targetCanvas.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D (Collider2D col)
+    {
+       
+        targetCanvas.gameObject.SetActive(true);
+    }
+}
